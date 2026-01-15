211fd698a114ddf3fc9654c61069679472487403
lizimu <lizimu@sensetime.com>
2026-01-15 21:58:19 +0800

    update for e2b_envd

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$DIR"/..
 
+# Start e2b envd server in background
+if [ -f /usr/bin/envd ]; then
+    /usr/bin/envd -isnotfc &
+fi
+
 make run-online HOST="''" PORT=${_BYTEFAAS_RUNTIME_PORT:-8080}
